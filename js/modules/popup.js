5be9fef268cca08474163c2c7ab1f8ff15b99b61
Andrii Konovalenko <an256.ak@gmail.com>
2020-09-05 23:06:39 +0300

    fix: fixed image in services, news

--- js/modules/popup.js
+++ js/modules/popup.js
@@ -1,27 +1,27 @@
 export const popup = () => {
 	const body = document.querySelector('body'),
-    popupCall = document.querySelector('.popup-call'),
-    popupPrice = document.querySelector('.popup-price');
+		popupCall = document.querySelector('.popup-call'),
+		popupPrice = document.querySelector('.popup-price');
 
 	body.addEventListener('click', event => {
 		const target = event.target;
 
 		if (target.matches('.popup-call-button')) {
 			popupCall.style.display = 'block';
-    }
-    
-    if (target.matches('.popup-price-button')) {
+		}
+
+		if (target.matches('.popup-price-button')) {
 			popupPrice.style.display = 'block';
-    }
-    
-    if (target.matches('.popup-price-call-button')) {
-      popupPrice.style.display = 'none';
-      popupCall.style.display = 'block';
-    }
+		}
+
+		if (target.matches('.popup-price-call-button')) {
+			popupPrice.style.display = 'none';
+			popupCall.style.display = 'block';
+		}
 
 		if (target.matches('.popup-close')) {
-      popupCall.style.display = 'none';
-      popupPrice.style.display = 'none';
+			popupCall.style.display = 'none';
+			popupPrice.style.display = 'none';
 		}
 	});
 };
